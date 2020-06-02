0012672458fa301709b967569ecbc6a801df59bb
Daichi Kanke <kanke@tam-tam.co.jp>
2020-06-02 17:38:26 +0900

    Workboxを使用したプリキャッシュ 追加

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
 self.addEventListener('fetch', function(e) {
   // ここは空でもOK
 })
+
+// ファイブラリのインポート
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '12345'
+  },
+  {
+    url: '/app.css',
+    revision: '12345'
+  },
+  {
+　url: '/app.js',
+    revision: '12345'
+  },
+])
